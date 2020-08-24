056713604f83c2c7f355d5c9cf54a1594deb06d1
arshadalis <arshad.ali.sayed.15@gmail.com>
2020-08-24 08:56:55 +0530

    add functional readings

--- src/main/scala/custom/monads/Demo.scala
+++ src/main/scala/custom/monads/Demo.scala
@@ -1,10 +1,13 @@
 package custom.monads
 
-object Demo extends App{
+object Demo extends App {
 
 
+  def increment: Int => Int = in => in + 1
 
+  def add[A](value: A, func: A => A) = func(value)
 
+  add(2, increment)
 
 
 }
--- target/scala-2.13/classes/demo.sc
+++
@@ -1,4 +0,0 @@
-
-
-
-println("HEllo TArique bhai")
